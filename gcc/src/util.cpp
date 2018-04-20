c66435dee5c5abfafd86244878b52a69dd5be85e
Alex <ols@dipgra.es>
2018-04-20 14:20:30 +0200

    Curl download

--- gcc/src/util.cpp
+++ gcc/src/util.cpp
@@ -147,6 +147,32 @@ bool UploadXML()
 	return exito;
 }
 
+bool DownloadXML()
+{
+	size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+	    size_t written = fwrite(ptr, size, nmemb, stream);
+	    return written;
+	}
+	    CURL *curl;
+	    FILE *fp;
+	    CURLcode res;
+	    char *url = "http://10.7.15.69/curl/files/impresoras.xml";
+	    char outfilename[FILENAME_MAX] = "./salida.xml";
+	    curl = curl_easy_init();
+	    if (curl) {
+	        fp = fopen(outfilename,"wb");
+	        curl_easy_setopt(curl, CURLOPT_URL, url);
+	        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+	        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+	        res = curl_easy_perform(curl);
+	        /* always cleanup */
+	        curl_easy_cleanup(curl);
+	        fclose(fp);
+	    }
+	    return 0;
+	}
+}
+
 /******************* LOG *****************************/
 
 //void log_handle(string log){
